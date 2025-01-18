32f03ec3753f79a1ab468e9670adeda6e295c6e7
40401241 <nwatkins02@qub.ac.uk>
2025-01-18 15:56:16 +0000

    Changed square types distribution and added further comments
    Changed the way square types are added to the array by using Collections.nCopies instead of a bunch of for loops.
    Also added more comments and formatted for readability.

--- src/main/java/BoardGame/Board.java
+++ src/main/java/BoardGame/Board.java
@@ -9,34 +9,36 @@ import java.util.Collections;
 
 /**
  * The board class represents the game board.
- * It extends JPanel and is responsible for initializing the board and randomly distributing the sqaure types.
+ * It extends JPanel and is responsible for initializing the board and randomly
+ * distributing the sqaure types.
  * The board is a grid with a changeable, fixed number of columns and rows.
  * 
  * @author Nathan Watkins
  */
 public class Board extends JPanel {
-    
+
     /**
      * The number of columns in the grid.
      */
     final int GRID_COLUMNS = 10;
 
     /**
-     * The number of rows in the grid.
+     * The number of rows in the grid (square board).
      */
-    final int GRID_ROWS = GRID_COLUMNS; // Currently set equal to GRID_COLUMNS to make it an even square
+    final int GRID_ROWS = GRID_COLUMNS;
 
     /**
      * Distributes the types of squares on the board based on a given ratio.
      * The types of squares include Knowledge, Pothole, Resource, and Normal.
      * Additionally, specific locations can be designated as Spawn points.
      * 
-     * @param totalSquares The total number of squares on the board.
+     * @param totalSquares   The total number of squares on the board.
      * @param spawnLocations A list of indices where spawn points should be placed.
      * @return A list of strings representing the type of each square on the board.
+     * @throws IllegalArgumentException if a spawn location is out of bounds.
      */
     public static List<String> gridDistribution(int totalSquares, List<Integer> spawnLocations) {
-        int[] distributionRatio = {1, 2, 2, 3}; //Knowledge : Pothole : Resource : Normal ratio
+        int[] distributionRatio = { 1, 2, 2, 3 }; // Knowledge : Pothole : Resource : Normal ratio
 
         int totalWeight = Arrays.stream(distributionRatio).sum();
         int numKnowledge = totalSquares / totalWeight * distributionRatio[0];
@@ -45,59 +47,56 @@ public class Board extends JPanel {
         int numNormal = totalSquares - (numKnowledge + numPothole + numResource);
 
         List<String> squareTypes = new ArrayList<>();
-        for (int i = 0; i < numNormal; i++) {
-            squareTypes.add("Normal");
-        }
-        for (int i = 0; i < numPothole; i++) {
-            squareTypes.add("Pothole");
-        }
-        for (int i = 0; i < numResource; i++) {
-            squareTypes.add("Resource");
-        }
-        for (int i = 0; i < numKnowledge; i++) {
-            squareTypes.add("Knowledge");
-        }
+
+        squareTypes.addAll(Collections.nCopies(numNormal, "Normal"));
+        squareTypes.addAll(Collections.nCopies(numPothole, "Pothole"));
+        squareTypes.addAll(Collections.nCopies(numResource, "Resource"));
+        squareTypes.addAll(Collections.nCopies(numKnowledge, "Knowledge"));
 
         Collections.shuffle(squareTypes);
 
         for (int location : spawnLocations) {
-            squareTypes.add(location, "Spawn");
+            if (location < 0 || location >= totalSquares) {
+                throw new IllegalArgumentException("Spawn location is out of bounds: " + location);
+            }
+            squareTypes.set(location, "Spawn");
         }
 
         return squareTypes;
     }
-    
+
     /**
-     * Constructs a new Board object and initializes the board with the default settings.
+     * Constructs a new Board object and initializes the board with the default
+     * settings.
      * The board is initialized with a grid of squares and specific spawn locations.
      */
     public Board() {
-        int totalSquares = GRID_COLUMNS*GRID_ROWS;
-        final List<Integer> spawnLocations = Arrays.asList(0, GRID_COLUMNS-1, totalSquares-GRID_COLUMNS, totalSquares-1);
+        int totalSquares = GRID_COLUMNS * GRID_ROWS;
+        final List<Integer> spawnLocations = Arrays.asList(0, GRID_COLUMNS - 1, totalSquares - GRID_COLUMNS,
+                totalSquares - 1);
         List<String> squareTypes = gridDistribution(totalSquares, spawnLocations);
 
         for (int i = 0; i < (totalSquares); i++) {
             JPanel panel = new JPanel();
-            panel.setPreferredSize(new Dimension(40, 40));
             panel.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
 
             String squareType = squareTypes.get(i);
             switch (squareType) {
                 case "Normal":
-                panel.setBackground(Color.WHITE);
-                break;
+                    panel.setBackground(Color.WHITE);
+                    break;
                 case "Pothole":
-                panel.setBackground(Color.RED);
-                break;
+                    panel.setBackground(Color.RED);
+                    break;
                 case "Knowledge":
-                panel.setBackground(Color.BLUE);
-                break;
+                    panel.setBackground(Color.BLUE);
+                    break;
                 case "Resource":
-                panel.setBackground(Color.ORANGE);
-                break;
+                    panel.setBackground(Color.ORANGE);
+                    break;
                 case "Spawn":
-                panel.setBackground(Color.GREEN);
-                break;
+                    panel.setBackground(Color.GREEN);
+                    break;
             }
 
             this.add(panel);
@@ -106,10 +105,11 @@ public class Board extends JPanel {
         this.setLayout(new java.awt.GridLayout(GRID_ROWS, GRID_COLUMNS));
     }
 
-
     /**
-     * Main method for running the board by itself.
-     * @param args
+     * The main method creates a JFrame and adds a new Board object to it.
+     * Can be used to run the game board as a standalone application.
+     * 
+     * @param args Command line arguments (not used).
      */
     public static void main(String[] args) {
         JFrame frame = new JFrame("Board Game");
