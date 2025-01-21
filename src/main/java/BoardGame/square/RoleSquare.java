b4b4277e059ee0e8e9bf53b6feb884ca90651e3a
40407018 <abogdanovs01@qub.ac.uk>
2025-01-21 15:54:19 +0000

    Update file RoleSquare.java

--- src/main/java/BoardGame/square/RoleSquare.java
+++ src/main/java/BoardGame/square/RoleSquare.java
@@ -1,20 +1,40 @@
 package square;
 
+import java.util.Random;
+
 public class RoleSquare extends Square{
 	private JobRole playerRole;
-	RoleSquare(JobRole role) {
-		this.playerRole = role;
+
+	RoleSquare() {
+		Random random = new Random();
+		// if in phase 2, add 4 to random number(assuming jobroles are numbered)
+		int roleReturned = random.nextInt(4);
+		playerRole.setJobRole(roleReturned);
 	}
-	
+
 	@Override
-	public void activateSquareEffect(Player p) {
-		boolean playerChoice = true;
-		//pop-up code
-		//print "do you want this role?"
-		
-		if (playerChoice) {
-			p.setJobRole(this.playerRole);
+	public void activateSquareEffect(Player p1, Player p2, Player p3, Player p4) {
+		// numbers 0-3 correspond with different job roles
+		// ask what player this role will be assigned to
+		int playerRoleAssignment;
+		switch (playerRoleAssignment) {
+		case 1:
+			p1.setJobRole(this.playerRole);
+			break;
+		case 2:
+			p2.setJobRole(this.playerRole);
+			break;
+		case 3:
+			p3.setJobRole(this.playerRole);
+			break;
+		case 4:
+			p4.setJobRole(this.playerRole);
+			break;
+		default:
+			//error; try again
 		}
 	}
-	
 }
+
+}
+
