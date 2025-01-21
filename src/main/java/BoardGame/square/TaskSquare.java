511dda88a6094a813d0789c5fbdfe52a6496932b
40400985 <kworthington01@qub.ac.uk>
2025-01-21 15:26:37 +0000

    moved TaskSquare class to square package

---
+++ src/main/java/BoardGame/square/TaskSquare.java
@@ -0,0 +1,19 @@
+package square;
+
+public class TaskSquare extends Square {
+	private Task task;
+
+	TaskSquare(Task ts) {
+		this.task = ts;
+	}
+
+	public boolean activateSquareTask(Player p) {
+		if (p.getRole() == task.getRole()) {
+			// pop-up code
+			task.doTask();// get task information
+		} else {
+			// print not correct role for task
+		}
+		//return true to sig
+	}
+}
