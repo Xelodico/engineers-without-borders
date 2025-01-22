22213b91bc06c8d9d35cc45fbff109a5f6d842a3
Nathan Watkins <nathan.wtkns@gmail.com>
2025-01-22 10:14:13 +0000

    Moved setVisible to BoardGameUI class.

--- src/main/java/BoardGame/GameSystem.java
+++ src/main/java/BoardGame/GameSystem.java
@@ -7,20 +7,19 @@ public class GameSystem {
 
     private Player player1 = new Player("Nathan", 0);
     private Player player2 = new Player("Curtis", 9);
+    private Player player3 = new Player("Isaac", 90);
 
     private static List<Player> players;
-
-    private static BoardGameUI game;
-
+    
     public GameSystem() {
         players = new ArrayList<>();
         players.add(player1);
         players.add(player2);
+        players.add(player3);
     }
 
     public static void main(String[] args) {
         new GameSystem();
-        game = new BoardGameUI(players);
-        game.setVisible(true);
+        new BoardGameUI(players);
     }
 }
