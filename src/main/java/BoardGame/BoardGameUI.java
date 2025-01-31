28b5329211cc74a58389f745d649f9d5a727b1a6
40401241 <nwatkins02@qub.ac.uk>
2025-01-31 10:04:01 +0000

    Removed test code

--- src/main/java/BoardGame/BoardGameUI.java
+++ src/main/java/BoardGame/BoardGameUI.java
@@ -1,6 +1,5 @@
 package BoardGame;
 
-import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -29,8 +28,6 @@ public class BoardGameUI extends JFrame {
     String player4Name;
 
     public StartScreen startScreen;
-
-    CardLayout cardLayout = new CardLayout();
 
     /**
      * Creates a new BoardGameUI instance with the specified list of players.
