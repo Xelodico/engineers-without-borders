08d28bef4cc2c70d6bec0b74ba517025cb716e5c
40405595 <iedmonds01@qub.ac.uk>
2025-02-27 19:20:55 +0000

    Update SubTaskTest.java
    Wrote out a test case for the discountSubTask() function

--- src/test/java/subtaskobjtest/SubTaskTest.java
+++ src/test/java/subtaskobjtest/SubTaskTest.java
@@ -2,13 +2,28 @@ package subtaskobjtest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import BoardGame.ResourceType;
+import BoardGame.SubTask;
 
 class SubTaskTest {
 
+	private SubTask subtask;
+	
+	@BeforeEach
+	void setUp() {
+		subtask = new SubTask("Research Funding Opportunities", 10, 25, ResourceType.INFLUENCE);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDiscount() {
+		System.out.println("Test");
+		subtask.discountSubTask();
+		assertEquals(5, subtask.getCompletionScore(), "completionScore should equal 5");
+		assertEquals(12, subtask.getResourceCost(), "resourceCost should equal 12");
 	}
 
+	
 }
