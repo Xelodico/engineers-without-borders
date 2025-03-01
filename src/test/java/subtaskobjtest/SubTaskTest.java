7a604715da7efcec2037423f39e608476bc70da7
40405595 <iedmonds01@qub.ac.uk>
2025-03-01 16:05:26 +0000

    Update SubTaskTest.java
    Added another test case and added authorship credit

--- src/test/java/subtaskobjtest/SubTaskTest.java
+++ src/test/java/subtaskobjtest/SubTaskTest.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 import BoardGame.ResourceType;
 import BoardGame.SubTask;
 
+/**
+ * JUnit test cases for the SubTask class
+ * 
+ * @author Isaac Edmonds
+ */
 class SubTaskTest {
 
 	private SubTask subtask;
@@ -19,11 +24,17 @@ class SubTaskTest {
 	
 	@Test
 	void testDiscount() {
-		System.out.println("Test");
 		subtask.discountSubTask();
 		assertEquals(5, subtask.getCompletionScore(), "completionScore should equal 5");
 		assertEquals(12, subtask.getResourceCost(), "resourceCost should equal 12");
 	}
 
-	
+	@Test
+	void testDiscountAlreadyApplied() {
+		subtask.setDiscounted(true);
+		subtask.discountSubTask();
+		assertTrue(subtask.isDiscounted(), "isDiscounted should be true");
+		assertEquals(10, subtask.getCompletionScore(), "completionScore should equal 10");
+		assertEquals(25, subtask.getResourceCost(), "resourceCost should equal 25");
+	}
 }
